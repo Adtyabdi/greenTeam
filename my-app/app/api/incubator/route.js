9a2e68df63975195bb2dfed83ab37550be068dd0
Adtyabdi <adtyabdii@gmail.com>
2024-10-07 20:41:22 +0700

    config db

--- my-app/app/api/incubator/route.js
+++ my-app/app/api/incubator/route.js
@@ -6,15 +6,12 @@ export async function GET(request) {
     const connection = await db();
     console.log('Database connected');
 
-    // Menjalankan query
     const [rows] = await connection.execute('SELECT * FROM incubator');
     console.log('Data fetched from database:', rows);
 
-    // Menutup koneksi
     await connection.end();
     console.log('Database connection closed');
 
-    // Mengirim data ke client
     return new Response(JSON.stringify(rows), {
       status: 200,
       headers: {
@@ -24,7 +21,6 @@ export async function GET(request) {
   } catch (error) {
     console.error('Error in API route:', error);
 
-    // Pastikan selalu mengembalikan response saat terjadi error
     return new Response(JSON.stringify({ message: 'Error fetching data', error: error.message }), {
       status: 500,
       headers: {
--- my-app/app/components/Header.jsx
+++ my-app/app/components/Header.jsx
@@ -4,34 +4,32 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <>
-    
-    <div className="bg-blue-500 w-full h-44 p-4">
-      <div className="flex justify-between items-center pt-3">
-      <Link href ="/dashboard">
-        <div className="text-3xl font-semibold">
-          <h1>Microgreen</h1>
-          <h1>Team</h1>
+    <>    
+      <div className="bg-blue-500 w-full h-44 p-4">
+        <div className="flex justify-between items-center pt-3">
+        <Link href ="/dashboard">
+          <div className="text-3xl font-semibold">
+            <h1>Microgreen</h1>
+            <h1>Team</h1>
+          </div>
+        </Link>
+          <Image src="/logo.png" width={70} height={70} alt="logo"/>
         </div>
-      </Link>
-        <Image src="/logo.png" width={70} height={70}/>
-      </div>
-      <div className="flex justify-between items-center gap-2 pt-4">
-        <div className="bg-amber-500 p-2 w-1/4 rounded-lg">
-          <h1 className="text-center">26°C</h1>
+        <div className="flex justify-between items-center gap-2 pt-4">
+          <div className="bg-amber-500 p-2 w-1/4 rounded-lg">
+            <h1 className="text-center">26°C</h1>
+          </div>
+          <div className="bg-amber-500 p-2 w-1/4 rounded-lg">
+            <h1 className="text-center">51% Rh</h1>
+          </div>
+          <div className="bg-amber-500 p-2 w-1/4 rounded-lg">
+            <h1 className="text-center">100 Lux</h1>
+          </div>
+          <div className="bg-amber-500 p-2 w-1/4 rounded-lg">
+            <h1 className="text-center">68 %</h1>
+          </div>
         </div>
-        <div className="bg-amber-500 p-2 w-1/4 rounded-lg">
-          <h1 className="text-center">51% Rh</h1>
-        </div>
-        <div className="bg-amber-500 p-2 w-1/4 rounded-lg">
-          <h1 className="text-center">100 Lux</h1>
-        </div>
-        <div className="bg-amber-500 p-2 w-1/4 rounded-lg">
-          <h1 className="text-center">68 %</h1>
-        </div>
-      </div>
-    </div>
-    
+      </div>    
     </>
   )
 }
