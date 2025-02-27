fcab4a4bbf0ae20fd6cd6e9f8f4018ea292d8e4d
Adtyabdi <adtyabdii@gmail.com>
2025-02-27 18:58:42 +0700

    last commit

--- app/api/batteryGet/route.js
+++ app/api/batteryGet/route.js
@@ -43,7 +43,6 @@ export async function GET(req) {
             LIMIT 10;
           `);
 
-
           const data = {
             latestBatteryData:
               rowsBattery.length > 0
--- app/api/getSave/route.js
+++ app/api/getSave/route.js
@@ -44,7 +44,6 @@ export async function GET(req) {
                 batteryVoltage, 
                 batteryPercentage,
                 temperatureCpanel, 
-                temperatureCbattery,
                 current, 
                 (batteryVoltage * current) AS power,
                 lux,
